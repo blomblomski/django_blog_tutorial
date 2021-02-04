928655c16021cfa4e2a88152b155183558bda3de
blomblomski <kevinplover@gmail.com>
2021-02-04 20:35:02 +0000

    blog

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'kevinplover@gmail.com'
+EMAIL_HOST_PASSWORD = 'PizzaGate2020'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
